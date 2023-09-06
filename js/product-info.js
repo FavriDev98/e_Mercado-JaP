363517a229201b2d564aa253301a334f6fde00db
emiliano soba <emisoba97@gmail.com>
2023-09-06 16:00:41 -0300

    punto3 emi

--- js/product-info.js
+++ js/product-info.js
@@ -7,3 +7,33 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     window.location.href = './login.html';
 })
+
+
+let product_localS = localStorage.getItem('prodID');
+
+const url = 'https://japceibal.github.io/emercado-api/products_comments/' + product_localS + '.json'
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    const arrayProductsCom = data.products_comments;
+    const dataContainer = document.getElementById('');
+
+    arrayProductsCom.forEach(item => {
+        const div = document.createElement('div')
+        div.classList.add('data-com')
+
+        div.innerHTML= `
+            <p>${item.user}</p>
+            <p>${item.dataTime}</p>
+            <p>${item.score}</p> `;
+
+        dataContainer.appendChild(div);
+
+    });
+})
+
+.catch(error => {
+    console.error('Error al obtener los datos:', error);
+});
