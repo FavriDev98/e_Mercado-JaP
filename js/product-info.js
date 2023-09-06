23ba3e538cc6a3a2ea183758ea207abc007b9b94
teageto <turnablees@gmail.com>
2023-09-06 16:56:51 -0300

    Se muestran los datos de los productos

--- js/product-info.js
+++ js/product-info.js
@@ -10,4 +10,31 @@ document.addEventListener("DOMContentLoaded", function(){
 
 let item_localS = localStorage.getItem('itemID');
 
-console.log(item_localS)
+const url = 'https://japceibal.github.io/emercado-api/products/' + item_localS + '.json';
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let product = data;
+        const dataContainer = document.getElementById('informacion-shw')
+
+            const div = document.createElement('div');
+            div.classList.add('info-co')
+
+            div.innerHTML = `
+                <h1>${product.name}</h1>
+                <hr>
+                <h2 class="product-title" > </h2>
+                <p>${product.currency} ${product.cost}</p>
+                <div class="description-container" >
+                <p>${product.description}</p>
+                <div class='showimg';>
+                <img src="${product.images}">
+                </div>
+                </div>
+            `;
+
+            dataContainer.appendChild(div);
+            
+});
