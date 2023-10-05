a783a8d7effee1fb7d4d9611164fb5187cee0aaa
FavriDev98 <favripn@gmail.com>
2023-10-04 21:24:54 -0300

    js cart punto 3

--- js/cart.js
+++ js/cart.js
@@ -22,3 +22,19 @@ document.addEventListener("DOMContentLoaded", function(){
     `    
     }
 })
+
+const urlUserCart = 'https://japceibal.github.io/user_cart/25801.json';
+document.addEventListener("DOMContentLoaded", function(){
+
+    let countBox = document.getElementById(/*id caja de cantidades*/);
+
+    countBox.value = urlUserCart.articles.count;
+})
+
+let countBox = document.getElementById(/*id caja de cantidades*/);
+countBox.addEventListener("change", function(){
+
+    let boxValue = document.getElementById(/*id de la caja de balor total*/);
+    boxValue.value = urlUserCart.articles.unitCost * countBox.value;
+
+})
