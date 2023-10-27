41ce27eb6793753eefcad2e0a5f19cd00aad50ec
Abril Umpierrez <abrilump@gmail.com>
2023-10-27 14:19:30 -0300

    campos de entrada deshabilitados

--- js/cart.js
+++ js/cart.js
@@ -105,15 +105,21 @@ const urlCarrito = 'https://japceibal.github.io/emercado-api/products/' + item +
     });
 });
 
-/*
 // habilitar o deshabilitar campos de entrada
 
 let tarjeta = document.getElementById("tarjeta");
 let transferencia = document.getElementById("transferencia");
+let numeroTarjeta = document.getElementById("numeroTarjeta");
+let codigoSeguridad = document.getElementById("codigoSeguridad");
+let vencimientoTarjeta = document.getElementById("vencimientoTarjeta");
+let cuentaBancaria = document.getElementById("cuentaBancaria");
+
 
 tarjeta.addEventListener("change", function() {
     if (tarjeta.checked) {
       transferencia.disabled = true;
+      cuentaBancaria.classList.add('is-disabled');
+      cuentaBancaria.readOnly = true;
     } else {
       transferencia.disabled = false;
     }
@@ -122,8 +128,13 @@ tarjeta.addEventListener("change", function() {
   transferencia.addEventListener("change", function() {
     if (transferencia.checked) {
       tarjeta.disabled = true;
+      numeroTarjeta.classList.add('is-disabled');
+      codigoSeguridad.classList.add('is-disabled');
+      vencimientoTarjeta.classList.add('is-disabled');
+      numeroTarjeta.readOnly = true;
+      codigoSeguridad.readOnly = true;
+      vencimientoTarjeta.readOnly = true;
     } else {
       tarjeta.disabled = false;
     }
   });
- */
