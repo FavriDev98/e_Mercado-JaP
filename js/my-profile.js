3342093a7cee1657dfaf6c0da4b5d73a57f3738f
teageto <turnablees@gmail.com>
2023-11-05 10:28:37 -0300

    no se pero si se

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function(){
     window.location.href = './login.html';
 });
 
+
+
 document.addEventListener("DOMContentLoaded", function(){
     let userShow = document.getElementById('user-name');
     let userData = localStorage.getItem('usuario');
@@ -21,16 +23,24 @@ document.addEventListener("DOMContentLoaded", function(){
             <p>${userSession}<p>
     `    
     }
+
+    if(profilePicStr != null){
+        profilePicture.onload = ()=>{
+            URL.revokeObjectURL(profilePicStr.src);
+        }
+    }
 });
 
+let profilePicStr = localStorage.getItem('profilePic');
 let profilePicture = document.getElementById('profilePicture');
 let fileUpload = document.getElementById('fileUpload');
   
 fileUpload.addEventListener("change", handleFiles, false);
-  
+
 function handleFiles() {
     for (let i = 0; i < this.files.length; i++) {
         profilePicture.src = URL.createObjectURL(this.files[i]);
+        localStorage.setItem('profilePic', profilePicture.src);
         profilePicture.width = 60;
         profilePicture.onload = () => {
             URL.revokeObjectURL(profilePicture.src);
@@ -45,4 +55,6 @@ let inputFirst = document.getElementById('inputFirst');
 let inputSecond = document.getElementById('inputSecond');
 let inputMail = document.getElementById('inputMail');
 let inputNumber = document.getElementById('inputNumber');
+
+inputMail.value = localStorage.getItem('usuario')
 
